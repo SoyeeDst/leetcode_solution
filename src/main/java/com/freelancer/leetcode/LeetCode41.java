e49ae81f6b57164021c7fd9ba593d1428b2edfa2
dengjun <dengjun@cd.tuan800.com>
2016-05-25 13:39:47 +0800

    Optimize solution to make it easy to be understood

--- src/main/java/com/freelancer/leetcode/LeetCode41.java
+++ src/main/java/com/freelancer/leetcode/LeetCode41.java
@@ -21,39 +21,25 @@ public class LeetCode41 {
             }
         }
 
-        if (min <= 0) {
+        if (min <= 0 || min >= 2) {
             return 1;
         }
 
         // Skip all some increments values and lift up the minimum values as well.
         int index = 0;
-        int minMax = min;
         Set<Integer> ignoredSet = new HashSet<>();
         do {
             if (nums[index] > min) {
-                // increment by one
-                if (nums[index] - minMax == 1) {
-                    minMax = nums[index];
-                } else {
-                    ignoredSet.add(nums[index]);
-                }
-
+                ignoredSet.add(nums[index]);
             }
             index++;
         } while (index < nums.length);
 
-        if (min >= 2) {
-            return 1;
-        }
-        if (ignoredSet.size() == 0) {
-            return minMax + 1;
-        }
-
         while(true) {
-            if (!ignoredSet.contains(minMax + 1)) {
-                return minMax + 1;
+            if (!ignoredSet.contains(min + 1)) {
+                return min + 1;
             }
-            minMax++;
+            min++;
         }
     }
 }
