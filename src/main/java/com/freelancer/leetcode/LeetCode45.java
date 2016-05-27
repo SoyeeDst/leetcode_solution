cd4c468fa6a9b1cbd73e779623b27208fd12e334
dengjun <dengjun@cd.tuan800.com>
2016-05-27 18:48:57 +0800

    Optimized version

--- src/main/java/com/freelancer/leetcode/LeetCode45.java
+++ src/main/java/com/freelancer/leetcode/LeetCode45.java
@@ -1,26 +1,61 @@
 package com.freelancer.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Soyee on 2016/5/26.
  */
 public class LeetCode45 {
 
     public int jump(int[] nums) {
+        Integer[] nullZeroList = nullZero(nums);
         if (nums.length == 1) {
             return 0;
         }
         if (nums.length == 2) {
             return 1;
         }
-        Integer[][] resultMatrix = new Integer[nums.length + 1][nums.length + 1];
-        performCalculation(resultMatrix, nums, 1, nums.length);
-        return resultMatrix[1][nums.length];
+        Integer[][] resultMatrix = new Integer[nullZeroList.length][nullZeroList.length];
+        performCalculation(resultMatrix, nullZeroList, 0, nullZeroList.length - 1);
+        return resultMatrix[0][nullZeroList.length - 1];
     }
 
-    // recursively do this to obtain the most optimized solution
-    private static void performCalculation(Integer[][] resultMatrix, int[] nums, int start, int end) {
+    private static Integer[] nullZero(int[] nums) {
+        List<Integer> numList = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != 0) {
+                numList.add(nums[index]);
+            }
+        }
+        return numList.toArray(new Integer[numList.size()]);
+    }
 
+    private static boolean flatNumbers(Integer[] nums) {
+        Set<Integer> uniqueData = new HashSet<>();
+        for (Integer num : nums) {
+            uniqueData.add(num);
+        }
+        return uniqueData.size() == 1 ? true : false;
+    }
+
+
+    // recursively do this to obtain the most optimized solution
+    private static void performCalculation(Integer[][] resultMatrix, Integer[] nums, int start, int end) {
+        //System.err.println("Perform calculation, start : " + start + ", end : " + end);
         if (resultMatrix[start][end] != null) {
+            // duplicate calculation, return directly
+            return;
+        }
+
+        if (flatNumbers(nums)) {
+            if ((nums.length - 1) % nums[0] == 0) {
+                resultMatrix[start][end] = (nums.length - 1) / nums[0];
+            } else {
+                resultMatrix[start][end] = ((nums.length - 1) / nums[0]) + 1;
+            }
             return;
         }
 
@@ -29,16 +64,14 @@ public class LeetCode45 {
             return;
         }
 
-        if (nums[start - 1] >= (end - start)) {
+        if (nums[start] >= (end - start)) {
             resultMatrix[start][end] = 1;
             return;
         }
 
-        if (end <= start + 1) {
-            if (nums[start - 1] >= 1) {
-                resultMatrix[start][end] = 1;
-                return;
-            }
+        if (end == start + 1) {
+            resultMatrix[start][end] = 1;
+            return;
         }
 
         int minimalStep = Integer.MAX_VALUE;
@@ -46,8 +79,8 @@ public class LeetCode45 {
         do {
             // recursively call calculation
             performCalculation(resultMatrix, nums, index, end);
-            performCalculation(resultMatrix, nums, start, index - 1);
-            int step = resultMatrix[start][index] + resultMatrix[index + 1][end];
+            performCalculation(resultMatrix, nums, start, index);
+            int step = resultMatrix[start][index] + resultMatrix[index][end];
             if (step < minimalStep) {
                 minimalStep = step;
             }
