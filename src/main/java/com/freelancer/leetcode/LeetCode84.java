c083397913da7ea29827c86dbc12578bbb833e73
dengjun <dengjun@cd.tuan800.com>
2016-06-12 16:02:16 +0800

    Correct algorithm

--- src/main/java/com/freelancer/leetcode/LeetCode84.java
+++ src/main/java/com/freelancer/leetcode/LeetCode84.java
@@ -6,54 +6,20 @@ package com.freelancer.leetcode;
  */
 public class LeetCode84 {
 
-    public int largestRectangleArea(int[] heights) {
-        if (heights == null || heights.length == 0) {
-            return 0;
-        }
-        int[] maxSizeFactors = new int[heights.length];
-        int max;
-        int[][] effectiveColumns = new int[heights.length][2];
-        maxSizeFactors[0] = heights[0];
-        effectiveColumns[0][0] = heights[0];
-        effectiveColumns[0][1] = 1;
-        max = heights[0];
-        int index = 1;
-        while (index < heights.length) {
-            int secondMax = Math.max(maxSizeFactors[index - 1], heights[index]);
-            MaximumFactors firstMax = evaluateTillMax(heights[index], index, effectiveColumns);
-            if (secondMax <= firstMax.sum) {
-                maxSizeFactors[index] = firstMax.sum;
-                // adjust previous elements
-                effectiveColumns[index][0] = firstMax.evenHeight;
-                effectiveColumns[index][1] = firstMax.count;
-            } else {
-                maxSizeFactors[index] = heights[index];
-                effectiveColumns[index][0] = heights[index];
-                effectiveColumns[index][1] = 1;
+    public int largestRectangleArea(int[] height) {
+        if (height == null || height.length < 1) return 0;
+        int[] stack = new int[height.length + 1];
+        int len = 0, max = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int h = (i == height.length) ? 0 : height[i];
+            while (len != 0 && (i == height.length || height[stack[len - 1]] > h)) {
+                if (len == 1)
+                    max = Math.max(height[stack[--len]] * i, max);
+                else
+                    max = Math.max(height[stack[--len]] * (i - stack[len - 1] - 1), max);
             }
-            if (maxSizeFactors[index] > max) {
-                max = maxSizeFactors[index];
-            }
-            index++;
+            stack[len++] = i;
         }
         return max;
-    }
-
-    public static MaximumFactors evaluateTillMax(int heights, int endOffset, int[][] effectiveColumns) {
-        int lastNumber = effectiveColumns[endOffset - 1][0];
-        int chosenHeights = Math.min(lastNumber, heights);
-        return new MaximumFactors(chosenHeights, (effectiveColumns[endOffset - 1][1] + 1));
-    }
-
-    private static class MaximumFactors {
-        int evenHeight;
-        int count;
-        int sum;
-
-        public MaximumFactors(int evenHeight, int count) {
-            this.evenHeight = evenHeight;
-            this.count = count;
-            this.sum = this.evenHeight * this.count;
-        }
     }
 }
--- src/test/java/com/freelancer/leetcode/LeetCode84Test.java
+++ src/test/java/com/freelancer/leetcode/LeetCode84Test.java
@@ -13,4 +13,28 @@ public class LeetCode84Test {
         LeetCode84 leetCode84 = new LeetCode84();
         Assert.assertEquals(10, leetCode84.largestRectangleArea(new int[] {2, 1, 5, 6, 2, 3}));
     }
+
+    @Test
+    public void testLargestRectangleArea2() {
+        LeetCode84 leetCode84 = new LeetCode84();
+        Assert.assertEquals(4, leetCode84.largestRectangleArea(new int[] {1, 2, 2}));
+    }
+
+    @Test
+    public void testLargestRectangleArea3() {
+        LeetCode84 leetCode84 = new LeetCode84();
+        Assert.assertEquals(3, leetCode84.largestRectangleArea(new int[] {2, 1, 2}));
+    }
+
+    @Test
+    public void testLargestRectangleArea4() {
+        LeetCode84 leetCode84 = new LeetCode84();
+        Assert.assertEquals(6, leetCode84.largestRectangleArea(new int[] {3, 2, 2}));
+    }
+
+    @Test
+    public void testLargestRectangleArea5() {
+        LeetCode84 leetCode84 = new LeetCode84();
+        Assert.assertEquals(9, leetCode84.largestRectangleArea(new int[] {1, 2, 3, 4, 5}));
+    }
 }
