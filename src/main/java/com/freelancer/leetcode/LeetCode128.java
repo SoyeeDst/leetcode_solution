ab840281a6937b2415870b356b19d7423f2295ac
soyee deng <soyee deng>
2016-07-11 21:29:44 +1200

    Optimized solution to get longest consecutive numbers(Comparing with last solution, the big memeory allocation will be avoided).

--- src/main/java/com/freelancer/leetcode/LeetCode128.java
+++ src/main/java/com/freelancer/leetcode/LeetCode128.java
@@ -1,7 +1,50 @@
 package com.freelancer.leetcode;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
- * Created by soyee on 2016/7/11.
+ * Created by Soyee on 2016/7/11.
  */
 public class LeetCode128 {
+
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int mt = Integer.MIN_VALUE;
+        Set<Integer> ds = new HashSet<>();
+        for (int n : nums) {
+            ds.add(n);
+        }
+
+        Iterator<Integer> iterator = ds.iterator();
+        while(iterator.hasNext()) {
+            int i = iterator.next();
+            ds.remove(i);
+            int c = i;
+            int x = 1;
+            while(ds.contains(++c)) {
+                ds.remove(c);
+                x++;
+            }
+            c = i;
+            while(ds.contains(--c)) {
+                ds.remove(c);
+                x++;
+            }
+            if (x > mt) {
+                mt = x;
+            }
+            // reassign
+            iterator = ds.iterator();
+        }
+        return mt;
+    }
 }
--- src/test/java/com/freelancer/leetcode/LeetCode128Test.java
+++ src/test/java/com/freelancer/leetcode/LeetCode128Test.java
@@ -1,7 +1,52 @@
 package com.freelancer.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * Created by soyee on 2016/7/11.
+ * Created by Soyee on 2016/7/11.
  */
 public class LeetCode128Test {
+
+    @Test
+    public void testLongestConsecutive() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(4, leetCode128.longestConsecutive(new int[] {100, 4, 200, 1, 3, 2}));
+    }
+
+    @Test
+    public void testLongestConsecutive2() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(5, leetCode128.longestConsecutive(new int[] {100, 4, 200, 1, 3, 2, 101, 102, 103, 104}));
+    }
+
+    @Test
+    public void testLongestConsecutive3() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(1, leetCode128.longestConsecutive(new int[] {100, 4, 200, 300, 250, 130, 380}));
+    }
+
+    @Test
+    public void testLongestConsecutive4() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(2, leetCode128.longestConsecutive(new int[] {0, -1}));
+    }
+
+    @Test
+    public void testLongestConsecutive5() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(5, leetCode128.longestConsecutive(new int[] {0, 1,-1,2, -2, 5,6, 7,8}));
+    }
+
+    @Test
+    public void testLongestConsecutive6() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(4, leetCode128.longestConsecutive(new int[] {0, 1, 2, 3,9 ,8, 20}));
+    }
+
+    @Test
+    public void testLongestConsecutive7() {
+        LeetCode128 leetCode128 = new LeetCode128();
+        Assert.assertEquals(3, leetCode128.longestConsecutive(new int[] {2147483646,-2147483647,0,2,2147483644,-2147483645,2147483645}));
+    }
 }
