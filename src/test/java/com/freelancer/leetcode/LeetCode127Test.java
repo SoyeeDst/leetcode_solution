b40d2e7746bcdbc568214b54b308bd76445a1f19
soyee deng <soyee deng>
2016-07-11 21:30:42 +1200

    Supplement some cases for LeetCode127

--- src/test/java/com/freelancer/leetcode/LeetCode127Test.java
+++ src/test/java/com/freelancer/leetcode/LeetCode127Test.java
@@ -1,7 +1,35 @@
 package com.freelancer.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by soyee on 2016/7/10.
+ * Created by Soyee on 2016/7/10.
  */
 public class LeetCode127Test {
+
+    @Test
+    public void testLadderLength() {
+        LeetCode127 leetCode127 = new LeetCode127();
+        Set<String> ds = new HashSet<>();
+        ds.add("hot");
+        ds.add("dot");
+        ds.add("dog");
+        ds.add("lot");
+        ds.add("log");
+        Assert.assertEquals(5,leetCode127.ladderLength("hit", "cog", ds));
+    }
+
+    @Test
+    public void testLadderLength2() {
+        LeetCode127 leetCode127 = new LeetCode127();
+        Set<String> ds = new HashSet<>();
+        ds.add("hot");
+        ds.add("dog");
+        Assert.assertEquals(0,leetCode127.ladderLength("hot", "dog", ds));
+    }
+
 }
