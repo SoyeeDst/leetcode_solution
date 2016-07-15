c7412d4d72c82fac97c52561250d6374e082e326
soyee deng <soyee deng>
2016-07-15 22:25:37 +1200

    Get all the unique partitioned palindromes of given string mainly via recursive searching forward, that could guarantee all the alternatives to be different from each other.

--- src/main/java/com/freelancer/leetcode/LeetCode131.java
+++ src/main/java/com/freelancer/leetcode/LeetCode131.java
@@ -1,7 +1,68 @@
 package com.freelancer.leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Created by soyee on 2016/7/14.
+ * Created by Soyee on 2016/7/14.
  */
 public class LeetCode131 {
+
+    public List<List<String>> partition(String s) {
+        if (s == null || s.equals("")) {
+            return Collections.emptyList();
+        }
+
+        if (s.length() == 1) {
+            return Collections.singletonList(Collections.singletonList(s));
+        }
+
+        return partitions(s, 0);
+    }
+
+    private static List<List<String>> partitions(String s, int start) {
+        if (start >= s.length()) {
+            return Collections.emptyList();
+        }
+        List<List<String>> pl = new ArrayList<>();
+        int sentinel = start;
+        do {
+            String clip = s.substring(start, sentinel + 1);
+            if (isPalindrome(clip)) {
+                List<List<String>> subPls = partitions(s, sentinel + 1);
+                if (subPls.isEmpty()) {
+                    // it has reached to the end of this string, retain the current one
+                    List<String> plAny = new ArrayList<>();
+                    plAny.add(clip);
+                    pl.add(plAny);
+                } else {
+                    for (List<String> subOption : subPls) {
+                        List<String> plAny = new ArrayList<>();
+                        plAny.add(clip);
+                        plAny.addAll(subOption);
+                        pl.add(plAny);
+                    }
+                }
+            }
+            sentinel++;
+        } while (sentinel < s.length());
+        return pl;
+    }
+
+    private static boolean isPalindrome(String part) {
+        if (part.length() == 1) {
+            return true;
+        }
+        int start = 0;
+        int end = part.length() - 1;
+        while(start <= end) {
+            if (part.charAt(start) != part.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
