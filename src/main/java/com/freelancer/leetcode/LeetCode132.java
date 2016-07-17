6b0c3121872e5cb690bdba16182a1f7403127c8e
soyee deng <soyee deng>
2016-07-17 18:56:26 +1200

    Add some quit condition for returning beforehand, and add one more intermediate matrix to cache some results during calculation as well, as the consequence, the performance should be several times(nearly 30) as before

--- src/main/java/com/freelancer/leetcode/LeetCode132.java
+++ src/main/java/com/freelancer/leetcode/LeetCode132.java
@@ -1,7 +1,69 @@
 package com.freelancer.leetcode;
 
 /**
- * Created by soyee on 2016/7/17.
+ * Created by Soyee on 2016/7/17.
  */
 public class LeetCode132 {
+
+    public int minCut(String s) {
+        if (s == null || s.equals("")) {
+            return 0;
+        }
+
+        if (s.length() == 1) {
+            return 0;
+        }
+
+        if (isPalindrome(s)) {
+            return 0;
+        }
+
+        Integer[][] m = new Integer[s.length()][s.length()];
+        return minParts(s, 0, s.length() - 1, m) - 1;
+    }
+
+    private static int minParts(String s, int start, int end, Integer[][] m) {
+        if (m[start][end] != null) {
+            return m[start][end];
+        }
+        if (isPalindrome(s.substring(start, end + 1))) {
+            m[start][end] = 1;
+            return 1;
+        }
+        int mc = Integer.MAX_VALUE;
+        int sentinel = end;
+        do {
+            String clip = s.substring(start, sentinel + 1);
+            if (isPalindrome(clip)) {
+                int parts = minParts(s, sentinel + 1, end, m);
+
+                if (parts + 1 < mc) {
+                    mc = parts + 1;
+                }
+
+                if (parts == 1) {
+                    break;
+                }
+            }
+            sentinel--;
+        } while (sentinel >= start);
+        m[start][end] = mc;
+        return mc;
+    }
+
+    private static boolean isPalindrome(String part) {
+        if (part.length() == 1) {
+            return true;
+        }
+        int start = 0;
+        int end = part.length() - 1;
+        while(start <= end) {
+            if (part.charAt(start) != part.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
