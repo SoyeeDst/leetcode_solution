4fb4b8fbc7e150b2d62a73e3e2dc4e1aa633e6c0
soyee deng <soyee deng>
2016-07-23 20:10:58 +1200

    Solve legacy TLE issue

--- src/main/java/com/freelancer/leetcode/LeetCode140.java
+++ src/main/java/com/freelancer/leetcode/LeetCode140.java
@@ -1,7 +1,57 @@
 package com.freelancer.leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 /**
- * Created by soyee on 2016/7/22.
+ * Created by Soyee on 2016/7/22.
  */
 public class LeetCode140 {
+
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        Boolean[][] throughFlags = new Boolean[s.length()][s.length()];
+        return wordBreak(s, wordDict, 0, throughFlags);
+    }
+
+    private static List<String> wordBreak(String s, Set<String> wordDict, int start, Boolean[][] throughFlags) {
+        if (start >= s.length()) {
+            return null;
+        }
+
+        if (throughFlags[start][s.length() - 1] != null && !throughFlags[start][s.length() - 1]) {
+            return Collections.emptyList();
+        }
+
+        // Calculate by recursive matching
+        List<String> pl = new ArrayList<>();
+        int sentinel = start;
+        do {
+            boolean oJ = false;
+            String clip = s.substring(start, sentinel + 1);
+            if (wordDict.contains(clip)) {
+                List<String> subPls = wordBreak(s, wordDict, sentinel + 1, throughFlags);
+                if (subPls == null) {
+                    // it has reached to the end of this string, retain the current one
+                    pl.add(clip);
+                } else {
+                    for (String subOption : subPls) {
+                        pl.add(clip + " " + subOption);
+                    }
+                }
+                if (subPls == null || !subPls.isEmpty()) {
+                    oJ = true;
+                }
+            }
+            if (throughFlags[start][s.length() - 1] == null) {
+                throughFlags[start][s.length() - 1] = oJ;
+            } else {
+                throughFlags[start][s.length() - 1] = throughFlags[start][s.length() - 1] | oJ;
+            }
+            sentinel++;
+        } while (sentinel < s.length());
+
+        return pl;
+    }
 }
--- src/test/java/com/freelancer/leetcode/LeetCode140Test.java
+++ src/test/java/com/freelancer/leetcode/LeetCode140Test.java
@@ -1,7 +1,73 @@
 package com.freelancer.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
- * Created by soyee on 2016/7/22.
+ * Created by Soyee on 2016/7/22.
  */
 public class LeetCode140Test {
+
+    @Test
+    public void testWordBreakSample() {
+        LeetCode140 leetCode140 = new LeetCode140();
+        Set<String> dict = new HashSet<>();
+        dict.add("cat");
+        dict.add("cats");
+        dict.add("and");
+        dict.add("sand");
+        dict.add("dog");
+        List<String> wordsList = leetCode140.wordBreak("catsanddog", dict);
+        Assert.assertEquals(2, wordsList.size());
+        Assert.assertEquals("cat sand dog", wordsList.get(0));
+        Assert.assertEquals("cats and dog", wordsList.get(1));
+    }
+
+    @Test
+    public void testWordBreakSample2() {
+        LeetCode140 leetCode140 = new LeetCode140();
+        Set<String> dict = new HashSet<>();
+        dict.add("a");
+        dict.add("abc");
+        dict.add("b");
+        dict.add("cd");
+        List<String> wordsList = leetCode140.wordBreak("abcd", dict);
+        Assert.assertEquals(1, wordsList.size());
+        Assert.assertEquals("a b cd", wordsList.get(0));
+    }
+
+    @Test
+    public void testWordBreakSample3() {
+        LeetCode140 leetCode140 = new LeetCode140();
+        Set<String> dict = new HashSet<>();
+        dict.add("aaaa");
+        dict.add("aa");
+        dict.add("a");
+        List<String> wordsList = leetCode140.wordBreak("aaaaaaa", dict);
+        Assert.assertEquals(31, wordsList.size());
+    }
+
+    @Test
+    public void testWordBreakSample4() {
+        LeetCode140 leetCode140 = new LeetCode140();
+        Long start = System.currentTimeMillis();
+        Set<String> dict = new HashSet<>();
+        dict.add("a");
+        dict.add("aa");
+        dict.add("aaa");
+        dict.add("aaaa");
+        dict.add("aaaaa");
+        dict.add("aaaaaa");
+        dict.add("aaaaaaa");
+        dict.add("aaaaaaaa");
+        dict.add("aaaaaaaaa");
+        dict.add("aaaaaaaaaa");
+        List<String> wordsList = leetCode140.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", dict);
+        Assert.assertEquals(0, wordsList.size());
+        System.err.println("Word break consumed : " + (System.currentTimeMillis() - start) + " ms");
+    }
 }
