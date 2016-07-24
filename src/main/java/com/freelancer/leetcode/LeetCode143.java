8fdc6c84cc9552064bfe1552debd51d8493f66af
soyee deng <soyee deng>
2016-07-24 12:12:04 +1200

    Resubmit

--- src/main/java/com/freelancer/leetcode/LeetCode143.java
+++ src/main/java/com/freelancer/leetcode/LeetCode143.java
@@ -1,7 +1,60 @@
 package com.freelancer.leetcode;
 
+import com.freelancer.leetcode.support.ListNode;
+
+import java.util.Stack;
+
 /**
- * Created by soyee on 2016/7/24.
+ * Created by Soyee on 2016/7/24.
  */
 public class LeetCode143 {
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+             return;
+        }
+
+        Stack<ListNode> sliceNode = slice(head);
+        ListNode node = head;
+        while (node != null) {
+            ListNode nextNode = node.next;
+            if (sliceNode.isEmpty()) {
+                 break;
+            }
+            insertNode(node, sliceNode.pop());
+            node = nextNode;
+        }
+    }
+
+    private static void insertNode(ListNode node, ListNode insertNode) {
+        ListNode nextNode = node.next;
+        node.next = insertNode;
+        insertNode.next = nextNode;
+    }
+
+    private static Stack<ListNode> slice(ListNode head) {
+        int cnr = 0;
+        ListNode node = head;
+        while (node != null) {
+            cnr++;
+            node = node.next;
+        }
+        int sliceCnt = cnr / 2;
+        int idx = 0;
+        node = head;
+        ListNode lastNode = null;
+        while (idx < cnr - sliceCnt) {
+            lastNode = node;
+            node = node.next;
+            idx++;
+        }
+        lastNode.next = null;
+        Stack<ListNode> stack = new Stack<>();
+        while (node != null) {
+            stack.push(node);
+            node = node.next;
+        }
+        return stack;
+    }
+
 }
--- src/test/java/com/freelancer/leetcode/LeetCode143Test.java
+++ src/test/java/com/freelancer/leetcode/LeetCode143Test.java
@@ -1,7 +1,61 @@
 package com.freelancer.leetcode;
 
+import com.freelancer.leetcode.support.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * Created by soyee on 2016/7/24.
+ * Created by Soyee on 2016/7/24.
  */
-public class LeetCode143 {
+public class LeetCode143Test {
+
+    @Test
+    public void testReorderList0() {
+        LeetCode143 leetCode143 = new LeetCode143();
+        ListNode listNode = new ListNode(1);
+        leetCode143.reorderList(listNode);
+        Assert.assertEquals(1, listNode.val);
+        ListNode ndNode = new ListNode(2);
+        listNode.next = ndNode;
+        leetCode143.reorderList(listNode);
+        Assert.assertEquals(1, listNode.val);
+        Assert.assertEquals(2, listNode.next.val);
+    }
+
+    @Test
+    public void testReorderList() {
+        LeetCode143 leetCode143 = new LeetCode143();
+        ListNode listNode = new ListNode(1);
+        ListNode ndNode = new ListNode(2);
+        ListNode rdNode = new ListNode(3);
+        ListNode fthNode = new ListNode(4);
+        listNode.next = ndNode;
+        ndNode.next = rdNode;
+        rdNode.next = fthNode;
+        leetCode143.reorderList(listNode);
+        Assert.assertEquals(1, listNode.val);
+        Assert.assertEquals(4, listNode.next.val);
+        Assert.assertEquals(2, listNode.next.next.val);
+        Assert.assertEquals(3, listNode.next.next.next.val);
+    }
+
+    @Test
+    public void testReorderList2() {
+        LeetCode143 leetCode143 = new LeetCode143();
+        ListNode listNode = new ListNode(1);
+        ListNode ndNode = new ListNode(2);
+        ListNode rdNode = new ListNode(3);
+        ListNode fthNode = new ListNode(4);
+        ListNode fithNode = new ListNode(5);
+        listNode.next = ndNode;
+        ndNode.next = rdNode;
+        rdNode.next = fthNode;
+        fthNode.next = fithNode;
+        leetCode143.reorderList(listNode);
+        Assert.assertEquals(1, listNode.val);
+        Assert.assertEquals(5, listNode.next.val);
+        Assert.assertEquals(2, listNode.next.next.val);
+        Assert.assertEquals(4, listNode.next.next.next.val);
+        Assert.assertEquals(3, listNode.next.next.next.next.val);
+    }
 }
