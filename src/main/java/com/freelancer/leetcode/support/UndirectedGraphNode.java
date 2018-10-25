d129df9bd565eeb3cba2e41b9d69d4589d3312af
Jun Deng <jun.deng@orionhealth.com>
2018-10-26 11:03:55 +1300

    DFS recursively deep copy a cyclic diagram

---
+++ src/main/java/com/freelancer/leetcode/support/UndirectedGraphNode.java
@@ -0,0 +1,16 @@
+package com.freelancer.leetcode.support;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UndirectedGraphNode {
+
+	public int label;
+	public List<UndirectedGraphNode> neighbors;
+
+	public UndirectedGraphNode(int x) {
+		label = x;
+		neighbors = new ArrayList<UndirectedGraphNode>();
+	}
+
+}
