22f9b10b27c93ae097f50080feac7cc02e511dcd
soyeedst <soyee.deng@gmail.com>
2018-11-21 13:59:57 +0800

    Find the indices which make up to be given target

---
+++ src/main/java/com/freelancer/leetcode/LeetCode167.java
@@ -0,0 +1,19 @@
+package com.freelancer.leetcode;
+
+public class LeetCode167 {
+
+	public int[] twoSum(int[] numbers, int target) {
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] + numbers[j] > target) {
+					break;
+				}
+				if (numbers[i] + numbers[j] == target) {
+					return new int[] { i + 1, j + 1 };
+				}
+			}
+		}
+		return null;
+	}
+
+}
